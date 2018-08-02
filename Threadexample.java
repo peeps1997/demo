a57721f34cf6931930aef4fd9053d6cf1815390c
ashekhawat <ashekhawat@BSC-SGH032QBS6.BLR.ALLEGISINDIA.COM>
2018-08-02 18:04:45 +0530

    minor changes

--- Threadexample.java
+++ Threadexample.java
@@ -1,38 +1,26 @@
 import java.util.LinkedList;
 import java.util.HashMap;
-public class Threadexample
-{
+public class Threadexample {
     public static void main(String[] args)
-                        throws InterruptedException
-    {
+    throws InterruptedException {
         final PC pc = new PC();
-        Thread t1 = new Thread(new Runnable()
-        {
+        Thread t1 = new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                try
-                {
+            public void run() {
+                try {
                     pc.produce();
-                }
-                catch(InterruptedException e)
-                {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
- 
-        Thread t2 = new Thread(new Runnable()
-        {
+
+        Thread t2 = new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                try
-                {
+            public void run() {
+                try {
                     pc.consume();
-                }
-                catch(InterruptedException e)
-                {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
@@ -42,50 +30,47 @@ public class Threadexample
         t1.join();
         t2.join();
     }
-    public static class PC
-    {
-        HashMap<String,Integer> list = new HashMap<String,Integer>();
+    public static class PC {
+        HashMap < String, Integer > list = new HashMap < String, Integer > ();
         int capacity = 2;
-        int value=1;
-        int i=1;
-        public void produce() throws InterruptedException
-        {
-            
-            while (true)
-            {
-                synchronized (this)
-                {   
-                    while ((list.get("consumer")==null)&&(i==0))
+        int value = 1;
+        int i = 1;
+        public void produce() throws InterruptedException {
+
+            while (true) {
+                synchronized(this) {
+                    while ((list.get("consumer") == null) && (i == 0))
                         wait();
- 
-                    System.out.println("Producer produced-"
-                                                  + this.value);
- 
-                    list.put("producer",this.value++);
-                    if(list.get("consumer")!=null){
-                    int val = list.remove("consumer");
-                    System.out.println("Producer consumed-"
-                    + val);}
+
+                    System.out.println("Producer produced-" +
+                        this.value);
+
+                    list.put("producer", this.value++);
+                    if (list.get("consumer") != null) {
+                        int val = list.remove("consumer");
+                        System.out.println("Producer consumed-" +
+                            val);
+                    }
                     notify();
-                    i=0;
+                    i = 0;
                     Thread.sleep(1000);
                 }
             }
         }
- 
-        public void consume() throws InterruptedException
-        {
-            while (true)
-            {
-                synchronized (this)
-                {
-                   try{ while (list.get("producer")==null)
-                        wait();
-                }catch(Exception e){System.out.println("Problems solved"+e);}
+
+        public void consume() throws InterruptedException {
+            while (true) {
+                synchronized(this) {
+                    try {
+                        while (list.get("producer") == null)
+                            wait();
+                    } catch (Exception e) {
+                        System.out.println("Problems solved" + e);
+                    }
                     int val = list.remove("producer");
-                    System.out.println("Consumer consumed-"+ val);
-                    list.put("consumer",this.value++);
-                    System.out.println("Consumer produced-"+ this.value);
+                    System.out.println("Consumer consumed-" + val);
+                    list.put("consumer", this.value++);
+                    System.out.println("Consumer produced-" + this.value);
                     notify();
                     Thread.sleep(1000);
                 }
--- src/CollectionExample.java
+++ src/CollectionExample.java
@@ -1,60 +1,60 @@
 import java.util.*;
-public class CollectionExample implements Comparator<String>{
-	
+public class CollectionExample implements Comparator < String > {
 
-	public static void main(String args[]) {
-		int i=0;
-		
-		List<String> strListArray=new ArrayList<String>();
-		List<String> strListLinked=new ArrayList<String>();
-		HashMap<Integer,String> strMap = new HashMap<Integer,String>();
-		if(strListArray.isEmpty()) 
-			if(strMap.isEmpty())
-				System.out.println("List n map empty");
-		while(i<10) {
-		strListArray.add("string Array List"+i);
-		strListLinked.add("string Linked List"+i);
-		strMap.put(i, "string Map "+i);
-		i++;
-		}
-		
-		if(strListArray.contains("string Array List1")) {
-			System.out.println("strListArray contains "+strListArray.get(1));
-		}
-		if(strListArray.equals(strListLinked)) {
-			System.out.println("strList equivalent to strLinked List");
-		}
-		Set set=strMap.entrySet();
-		Iterator j = set.iterator();
-		Iterator x = strListArray.iterator();
-		
-		System.out.println("strMap hashcode: "+strMap.hashCode());
-		System.out.println("Set :"+set);
-		Collections.shuffle(strListLinked);
-		Collections.shuffle(strListArray);
-		Collections.sort(strListLinked, new CollectionExample());
-		Iterator y = strListLinked.iterator();
-		while(j.hasNext()) {
 
-			System.out.println("StrList Map "+j.next());
-			j.remove();
-			System.out.println("StrList Array "+x.next());
-			x.remove();
-			System.out.println("StrList Linked "+y.next());
-			y.remove();
-		}
-		if(strListArray.isEmpty()) {
-			System.out.println("Array List empty");
-		}
+    public static void main(String args[]) {
+        int i = 0;
 
-		if(strMap.isEmpty()) {
-			System.out.println("Map empty");
-		}
-	}
+        List < String > strListArray = new ArrayList < String > ();
+        List < String > strListLinked = new ArrayList < String > ();
+        HashMap < Integer, String > strMap = new HashMap < Integer, String > ();
+        if (strListArray.isEmpty())
+            if (strMap.isEmpty())
+                System.out.println("List n map empty");
+        while (i < 10) {
+            strListArray.add("string Array List" + i);
+            strListLinked.add("string Linked List" + i);
+            strMap.put(i, "string Map " + i);
+            i++;
+        }
 
-	@Override
-	public int compare(String arg0, String arg1) {
-		
-		return arg0.compareTo(arg1);
-	}
+        if (strListArray.contains("string Array List1")) {
+            System.out.println("strListArray contains " + strListArray.get(1));
+        }
+        if (strListArray.equals(strListLinked)) {
+            System.out.println("strList equivalent to strLinked List");
+        }
+        Set set = strMap.entrySet();
+        Iterator j = set.iterator();
+        Iterator x = strListArray.iterator();
+
+        System.out.println("strMap hashcode: " + strMap.hashCode());
+        System.out.println("Set :" + set);
+        Collections.shuffle(strListLinked);
+        Collections.shuffle(strListArray);
+        Collections.sort(strListLinked, new CollectionExample());
+        Iterator y = strListLinked.iterator();
+        while (j.hasNext()) {
+
+            System.out.println("StrList Map " + j.next());
+            j.remove();
+            System.out.println("StrList Array " + x.next());
+            x.remove();
+            System.out.println("StrList Linked " + y.next());
+            y.remove();
+        }
+        if (strListArray.isEmpty()) {
+            System.out.println("Array List empty");
+        }
+
+        if (strMap.isEmpty()) {
+            System.out.println("Map empty");
+        }
+    }
+
+    @Override
+    public int compare(String arg0, String arg1) {
+
+        return arg0.compareTo(arg1);
+    }
 }
